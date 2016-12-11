e7c13d6e33e2bb42186f10c67778ba6e36a7f045
Christopher <crcorrell@gmail.com>
2016-12-10 22:09:03 -0600

    Implemented Expense Class

--- Expense.hpp
+++ Expense.hpp
@@ -1,7 +1,23 @@
+/*
+* Chris Correll 2016 
+* A class to represent an Expense. This class containes the cost of each item and the
+* people splitting it and provides functions to calculate the cost each person owes on the item
+*/
+
+//Includes
+#include <string>
+
 class Expense
 {
     Public:
-        std::string name;
-        vector <Person> purchasers;
+        Expense();
+        virtual ~Expense();  //why virtual
+        
+        float const Expense::det_cost() const
+        {
+            return cost/(*purchasers).size(); 
+        }
+                
+        std::vector <Person> purchasers;
         float cost;  
 }
